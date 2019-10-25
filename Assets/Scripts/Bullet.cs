92128af8d9e451fdb37335d0801ad38341408938
DESKTOP-R5B7G68\Marcin <marjackarpinski@gmail.com>
2019-10-25 23:44:32 +0200

    shooting makes spheres that travel from end of gun to the end of raycast from camera

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float bulletSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    private float timeBetweenBullets = 0.15f;
+    private float lastBulletTime;
+
+    [SerializeField]
+    private Transform bulletStartingPoint;
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    private void Start()
+    {
+        lastBulletTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && lastBulletTime > timeBetweenBullets)
+        {
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.transform.gameObject);
+                var bulletObject = Instantiate(bulletPrefab, bulletStartingPoint.position, Quaternion.identity);
+                bulletObject.transform.LookAt(hit.point);
+            }
+            lastBulletTime = 0;
+        }
+        lastBulletTime += Time.deltaTime;
+    }
+}
