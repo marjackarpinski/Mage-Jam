6dd0b221321e5a78ceb875a34e7cd6d54d3e242b
DESKTOP-R5B7G68\Marcin <marjackarpinski@gmail.com>
2019-10-26 07:29:37 +0200

    rotating environment

--- Assets/Scripts/GlobalGameEffects.cs
+++ Assets/Scripts/GlobalGameEffects.cs
@@ -7,6 +7,12 @@ public class GlobalGameEffects : MonoBehaviour
 
     public static Vector3 defaultGravity;
 
+    [SerializeField]
+    private Transform envirounment;
+    private float minRotationOffset = 1;
+    private bool isRotating = false;
+    private Vector3 targetRotation = Vector3.zero;
+
     private void Start()
     {
         if (defaultGravity != null)
@@ -28,30 +34,48 @@ public class GlobalGameEffects : MonoBehaviour
         Physics.gravity = defaultGravity;
     }
 
+    public void RotateWorld(Vector3 rotationValue)
+    {
+        Debug.Log("Starting Rotation");
+        isRotating = true;
+        targetRotation = envirounment.rotation.eulerAngles + rotationValue;
+    }
+
     private void Update()
     {
+        if(isRotating)
+        {
+            Debug.Log($"IsRotating, enviounmentRotation {envirounment.rotation.eulerAngles}, targetRotation {targetRotation}");
+            envirounment.rotation = Quaternion.Euler(Vector3.Lerp(envirounment.rotation.eulerAngles, targetRotation, 0.05f));
+            if((envirounment.rotation.eulerAngles - targetRotation).magnitude < minRotationOffset)
+            {
+                envirounment.rotation = Quaternion.Euler(targetRotation);
+                isRotating = false;
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.M))
         {
-            ChangeTimeScale(0.5f);
+            RotateWorld(new Vector3(0, 0, 90));
         }
 
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            ChangeTimeScale(1);
-        }
+        //if (Input.GetKeyDown(KeyCode.N))
+        //{
+        //    ChangeTimeScale(1);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            SetGravityLevel(0);
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            SetGravityLevel(0.5f);
-        }
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            RestoreDefaultGravity();
-        }
+        //if (Input.GetKeyDown(KeyCode.B))
+        //{
+        //    SetGravityLevel(0);
+        //}
+        //if (Input.GetKeyDown(KeyCode.V))
+        //{
+        //    SetGravityLevel(0.5f);
+        //}
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    RestoreDefaultGravity();
+        //}
     }
 
 
