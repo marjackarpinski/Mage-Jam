9402eb78b1420d0a4a69794b4e7b6d98e7488be8
Shape <lukasz.motowidlo@protonmail.com>
2019-10-26 07:12:00 +0200

    chodzi, nie odpierdala maniany dzieki

---
+++ Assets/Scripts/EnemyRunning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRunning : MonoBehaviour
+{ 
+
+    [SerializeField]
+    GameObject player;
+
+    void FixedUpdate()
+        {
+       
+            Vector3 lookVector = player.transform.position - transform.position;
+            lookVector.y = transform.position.y;
+            Quaternion rot = Quaternion.LookRotation(lookVector);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+            transform.Translate(Vector3.forward*Time.deltaTime * 3);
+
+        }
+
+}
+
+
+
+
